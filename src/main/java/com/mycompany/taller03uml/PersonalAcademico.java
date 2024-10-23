8cb9763d12172695b89a1d277db0942bfe8bbafc
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-10-23 14:36:44 -0500

    Create PersonalAcademico.java

---
+++ src/main/java/com/mycompany/taller03uml/PersonalAcademico.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03uml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class PersonalAcademico {
+    
+    public void reportarError(String email, String contenido){
+        //Hace algo
+   
+    }
+    public void solicitarAsesoramientoDelSistema(String email, String contenido){
+        //Hace algo
+   
+    }
+    
+    public void comentarEnForo(Foro foro, String comentario){
+        //Hace algo
+   
+    }
+}
