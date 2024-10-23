fde394e3ee1ad1c073d983b660b440b4ac0fd874
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-10-23 14:57:13 -0500

    Create Evaluacion.java

---
+++ src/main/java/com/mycompany/taller03uml/Evaluacion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03uml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Evaluacion extends ActividadSumativa {
+    
+    private int limiteDeTiempo;
+}
