eef7bea982daba7184e5f5f100f771e80d0080b6
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-10-23 14:15:33 -0500

    Taller03
    agregar el java

---
+++ src/main/java/com/mycompany/taller03uml/Taller03UML.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller03uml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller03UML {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
