cef45be79d4a822bafabb53d175f0e6ce6a966f4
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-10-23 14:37:10 -0500

    Create Profesor.java

---
+++ src/main/java/com/mycompany/taller03uml/Profesor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03uml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Profesor {
+    
+    public void cargarActividadSumativa(Curso curso, ActividadSumativa actividadsumativa){
+        //Hace algo
+    }
+    public void calificarActividadSumativa(Curso curso, String usuarioEstudiante){
+        //Hace algo
+    }
+    
+    public void aceptarInscripcion(Curso curso, String usuarioEstudiante){
+        //Hace algo
+    }
+    
+    public void crearNuevoForo(Foro foro){
+        //Hace algo
+    }
+}
