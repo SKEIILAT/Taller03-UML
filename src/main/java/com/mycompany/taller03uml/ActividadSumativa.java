a1a620d3faecb2dbbb0b1c9a2ec328f4c0d6110d
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-10-23 14:53:57 -0500

    Create ActividadSumativa.java

---
+++ src/main/java/com/mycompany/taller03uml/ActividadSumativa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03uml;
+
+/**
+ *
+ * @author CltControl
+ */
+public abstract class ActividadSumativa {
+    
+    protected String titulo;
+    protected DataTime fechaDeEntrega;
+    protected float puntajeMaximo;
+    protected String contenido;
+    protected float calificacion;
+    protected String id;
+}
